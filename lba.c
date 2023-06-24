56a123b2a80dea4edaa4dc036c32c4d3bc44d43c
JISHNU J S <102861844+JISHNU-2002@users.noreply.github.com>
2023-06-24 09:29:01 +0530

    Add files via upload

---
+++ lba.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n,bsize,orate,input[50],store = 0;
+    printf("Enter bucket size            = ");
+    scanf("%d",&bsize);    
+    printf("Enter outgoing rate          = ");
+    scanf("%d",&orate);
+    printf("Enter no of incoming packets = ");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;i++){
+        printf("\nEnter packet[%d] size  = ",i);
+        scanf("%d",&input[i]);
+
+        if(input[i] <= (bsize-store)){
+            store = store + input[i];
+            printf("[%d] bytes in [%d] bucket area\n",store,bsize);
+        }else{
+            int drop = input[i] - (bsize - store);
+            printf("dropped [%d] bytes from [%d] input\nbucket area is full [%d]/[%d]\n",drop,input[i],bsize,bsize);
+            store = bsize;        
+        }
+        store = store - orate;
+        printf("[%d] bytes left in bucket after transmition of [%d] input\n",store,input[i]);
+    }
+    return 0;
+}
