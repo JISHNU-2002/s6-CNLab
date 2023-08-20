02444bfd4eaf04deeb68ab58f3299b64d012541b
JISHNU J S <102861844+JISHNU-2002@users.noreply.github.com>
2023-08-20 12:54:47 +0530

    Update dvr_bellmanford.c

--- dvr_bellmanford.c
+++ dvr_bellmanford.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 struct node{
-    int from[10];
+    int to[10];
     int d[10];
 }rtab[10];
 
 int main(){
-    int costmatrix[10][10],i,j,k,n,count=0;
+    int costmatrix[10][10],i,j,k,n;
     printf("Enter the no of nodes = ");
     scanf("%d",&n);
     printf("Enter the cost matrix\n'We consider undirected graph case'\n");
@@ -15,24 +14,21 @@ int main(){
         for(j=0;j<n;j++){
             scanf("%d",&costmatrix[i][j]);
             costmatrix[i][i] = 0;
-            rtab[i].from[j] = j;
+            rtab[i].to[j] = j;
             rtab[i].d[j] = costmatrix[i][j];
         }
     }
-    do{
-        count = 0;
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
-                for(k=0;k<n;k++){
-                    if(rtab[i].d[j] > costmatrix[i][k] + rtab[k].d[j]){
-                        rtab[i].from[j] = k;
-                        rtab[i].d[j] = rtab[i].d[k] + rtab[k].d[j];
-                        count++;
-                    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            for(k=0;k<n;k++){
+                if(rtab[i].d[j] > rtab[i].d[k] + rtab[k].d[j]){
+                    rtab[i].to[j] = k;
+                    rtab[i].d[j] = rtab[i].d[k] + rtab[k].d[j];
                 }
             }
         }
-    }while(count != 0);
+    }
 
     for(i=0;i<n;i++){
         printf("\nRouting Table of %d\n------------------\nfrom | \tto | \tdt\n------------------\n",i);
